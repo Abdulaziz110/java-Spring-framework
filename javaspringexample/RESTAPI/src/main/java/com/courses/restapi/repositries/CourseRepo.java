4918b4178aa73ff5d1830105f014a04173b11dea
Abdulaziz110 <95578085+Abdulaziz110@users.noreply.github.com>
2023-08-14 12:05:09 +0300

    Delete javaspringexample directory

--- javaspringexample/RESTAPI/src/main/java/com/courses/restapi/repositries/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.courses.restapi.repositries;
-
-import com.courses.restapi.entities.courses.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CourseRepo extends JpaRepository<Course,Long> {
-}
--- javaspringexample/RESTAPI/src/main/java/com/courses/restapi/repositries/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.courses.restapi.repositries;
-
-import com.courses.restapi.entities.Students.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student,Long> {
-
-}
