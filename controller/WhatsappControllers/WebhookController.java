28f5cbe87d716693da27804b983aec4be1ef4fcb
Abdulaziz110 <95578085+Abdulaziz110@users.noreply.github.com>
2023-08-21 22:27:51 +0300

    Update WebhookController.java
    a

--- controller/WhatsappControllers/WebhookController.java
+++ controller/WhatsappControllers/WebhookController.java
@@ -1,13 +1,24 @@
 package com.example.demo.controller.WhatsappControllers;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebhookController {
 
-    @PostMapping("/webhook")
-    public void handleWebhook(@RequestBody String payload) {
-        System.out.println(payload);
+    // This endpoint handles the verification request
+    @GetMapping("/webhooks")
+    public String handleVerification(
+            @RequestParam("hub.mode") String mode,
+            @RequestParam("hub.challenge") String challenge,
+            @RequestParam("hub.verify_token") String verifyToken) {
+
+        if ("subscribe".equals(mode) && "meatyhamhock".equals(verifyToken)) {
+            // Respond with the challenge value to verify the webhook
+            return challenge;
+        } else {
+            // Invalid verification request
+            return "Invalid request";
+        }
     }
 }
