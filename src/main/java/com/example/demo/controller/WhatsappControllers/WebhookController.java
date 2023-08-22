43b92fcf2622e229a08abc29b2fe2991deaf9f20
Abdulaziz110 <95578085+Abdulaziz110@users.noreply.github.com>
2023-08-22 10:07:47 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/WhatsappControllers/WebhookController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller.WhatsappControllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+
+    private final String myToken = "1q2w3e";
+
+    @GetMapping("/webhook")
+    public ResponseEntity<String> handleWebhookVerification(
+            @RequestParam("hub.mode") String mode,
+            @RequestParam("hub.challenge") String challenge,
+            @RequestParam("hub.verify_token") String verifyToken) {
+
+        if (mode != null && verifyToken != null) {
+            if ("subscribe".equals(mode) && myToken.equals(verifyToken)) {
+                return new ResponseEntity<>(challenge, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
+
