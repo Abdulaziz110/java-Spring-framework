2006b266aa105cd545e3ca508586ad1ef87b1b36
Abdulaziz110 <95578085+Abdulaziz110@users.noreply.github.com>
2023-08-22 10:25:58 +0300

    Update WebhookController.java

--- src/main/java/com/example/demo/controller/WhatsappControllers/WebhookController.java
+++ src/main/java/com/example/demo/controller/WhatsappControllers/WebhookController.java
@@ -3,6 +3,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +12,39 @@ public class WebhookController {
 
     private final String myToken = "1q2w3e";
 
-    @PostMapping("/webhook")
-    public ResponseEntity<String> handleWebhookVerification(
+    @GetMapping("/webhook")
+    public ResponseEntity<WebhookResponse> handleWebhookVerification(
             @RequestParam("hub.mode") String mode,
             @RequestParam("hub.challenge") String challenge,
             @RequestParam("hub.verify_token") String verifyToken) {
 
+        WebhookResponse response = new WebhookResponse();
+
         if (mode != null && verifyToken != null) {
             if ("subscribe".equals(mode) && myToken.equals(verifyToken)) {
-                return new ResponseEntity<>(challenge, HttpStatus.OK);
+                response.setResponse(challenge);
+
+                return new ResponseEntity<>(response, HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+                response.setResponse("Invalid request");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             }
         } else {
-            return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+            response.setResponse("Invalid request");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+        
+    }
+
+     class WebhookResponse {
+        private String response;
+
+        public String getResponse() {
+            return response;
+        }
+
+        public void setResponse(String response) {
+            this.response = response;
         }
     }
 }
