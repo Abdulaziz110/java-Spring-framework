181a301df90350e26c8bf17a8ae593c2a193f1fb
Abdulaziz110 <95578085+Abdulaziz110@users.noreply.github.com>
2023-08-26 14:21:25 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/domin/CountryWeather.java
@@ -0,0 +1,17 @@
+package com.example.demo.domin;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CountryWeather {
+
+    private String country_name;
+    private double temp;
+    private double main_temp;
+    private double feels_like;
+    private double humidity;
+
+}
